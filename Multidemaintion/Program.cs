152365457d6454ebc7fd4b54e1d5f9fb310e500e
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-21 14:47:13 +0530

    Add project files.

---
+++ Multidemaintion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multidemaintion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 4]
+            {
+                {1, 2, 3, 4},
+                {5,6, 7, 8},
+                {9,10, 11, 12},
+            };
+
+           foreach(var i in arr)
+            {
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
